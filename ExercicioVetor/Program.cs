a509579853a18637496b75f3d05a16aeb2943d82
Elton Tozatto <eltontozatto@gmail.com>
2020-05-11 13:43:55 -0300

    Execucao da aula de vetores

---
+++ ExercicioVetor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExercicioVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Estudante[] vectEstudante = new Estudante[10];
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vectEstudante[room] = new Estudante { Name = name, Email = email };
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms:");
+            for(int i = 0; i < 10; i++)
+            {
+                if(vectEstudante[i] != null)
+                    Console.WriteLine(i + ": " + vectEstudante[i]);
+            }
+        }
+    }
+}
